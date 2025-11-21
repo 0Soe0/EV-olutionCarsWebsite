aed53ce490ba89287d7671b3316d10cbd176a983
Soe <so-phi-e@hotmail.com>
2025-11-20 21:08:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Background } from './components/Background/Background';
+import { Navbar } from './components/Navbar/Navbar';
+import { Hero } from './components/Hero/Hero';
+import { useState, useEffect } from 'react';
+
+export const App = () => {
+
+let heroData = [
+  {text1: "Dive into", text2: "what you love"},
+  {text1: "Indulge", text2: "your passions"},
+  {text1: "Give into", text2: "your dreams"}
+];
+
+const [heroCount, setHeroCount] = useState(2);
+const [playStatus, setPlayStatus] = useState(false);
+
+useEffect(() => {
+ setInterval(() => {
+  setHeroCount((count) => {return count === 2 ? 0 : count + 1;});
+ }, 3000);
+}, []);
+
+  return (
+    <div>
+      <Background 
+      playStatus={playStatus} 
+      heroCount={heroCount}/>
+
+      <Navbar />
+
+      <Hero 
+      heroData={heroData[heroCount]}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+      setPlayStatus={setPlayStatus}
+      />
+    </div>
+  )
+}
